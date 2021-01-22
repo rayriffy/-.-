d0f9975278a8511626006fafdee9f5003748538f
Phumrapee Limpianchop <git@rayriffy.com>
2021-01-22 13:08:09 +0900

    chore: staged changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, useRef } from 'react'
 
-import { NextPage } from 'next'
+import { GetStaticProps, NextPage } from 'next'
+import Head from 'next/head'
 
 import { useViewportSize } from 'web-api-hooks'
 
@@ -16,13 +17,19 @@ interface GeneratedFigure {
     width: number
     height: number
   }
-  position: {
+  position?: {
     width: number
     height: number
   }
 }
 
-const Page: NextPage = props => {
+interface Props {
+  intitialRiffy: GeneratedFigure
+}
+
+const Page: NextPage<Props> = props => {
+  const { intitialRiffy } = props
+
   const [width, height] = useViewportSize()
 
   const [figures, setFigures] = useState<GeneratedFigure[]>([])
@@ -61,22 +68,54 @@ const Page: NextPage = props => {
   }, [width, height])
 
   return (
-    <main className="bg-gray-50 h-full relative overflow-hidden">
-      {figures.map(figure => (
+    <React.Fragment>
+      <Head>
+        <link rel="preload" as="image" href={`/static/${intitialRiffy.file}`} />
+      </Head>
+      <main className="bg-gray-50 h-full relative overflow-hidden flex justify-center items-center">
         <img
-          key={`figure-${figure.id}`}
-          src={`/static/${figure.file}`}
-          width={`${Math.floor(figure.image.width / 2)}`}
+          key={`figure-intitial-${intitialRiffy.id}`}
+          src={`/static/${intitialRiffy.file}`}
+          width={`${Math.floor(intitialRiffy.image.width / 2)}`}
           height="auto"
-          style={{
-            position: 'absolute',
-            left: figure.position.width,
-            top: figure.position.height,
-          }}
         />
-      ))}
-    </main>
+        {figures.map(figure => (
+          <img
+            key={`figure-${figure.id}`}
+            src={`/static/${figure.file}`}
+            width={`${Math.floor(figure.image.width / 2)}`}
+            height="auto"
+            style={{
+              position: 'absolute',
+              left: figure.position.width,
+              top: figure.position.height,
+            }}
+          />
+        ))}
+      </main>
+    </React.Fragment>
   )
+}
+
+export const getServerSideProps: GetStaticProps<Props> = async context => {
+  const { nanoid } = await import('nanoid')
+  const { sample } = await import('lodash')
+  const { figures } = await import('../core/constants/figures')
+
+  const targetFigure = sample(figures)
+
+  return {
+    props: {
+      intitialRiffy: {
+        id: nanoid(),
+        file: targetFigure.file,
+        image: {
+          width: targetFigure.width,
+          height: targetFigure.height,
+        },
+      },
+    },
+  }
 }
 
 export default Page
